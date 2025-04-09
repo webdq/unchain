077326304d834d7d10bbf6574cafdb0a007bbfb2
mojocn <neochau@gmail.com>
2025-04-09 09:31:45 +0800

    simplify

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -11,7 +11,6 @@ ssh -o StrictHostKeyChecking=no -i key.pem $SSH_USER@$SSH_HOST << EOF
   sudo chmod +x /app/unchain
   echo "$CONFIG_TOML" | sudo tee /app/config.toml > /dev/null
   sudo mv unchain.service /etc/systemd/system/unchain.service
-  sudo rm -rf /etc/systemd/system/emissary.service
   sudo systemctl daemon-reload
   sudo systemctl stop unchain.service || true
   sudo systemctl start unchain.service
